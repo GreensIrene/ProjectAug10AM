1373680d4298020c604dd4cccd31e98159001764
GreensIrene <albeenairene28@gmail.com>
2021-08-24 10:47:44 +0530

    LoadUrl DOne

--- src/org/resources/LibGlobal.java
+++ src/org/resources/LibGlobal.java
@@ -2,4 +2,12 @@ package org.resources;
 
 public class LibGlobal {
 
+	public static void loadUrl() {
+		System.out.println("Load url");
+
+	}
+	
+	public static void main(String[] args) {
+		loadUrl();
+	}
 }
